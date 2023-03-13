b790a9764c36248450317859feb3bc8881222a1e
Otuslettia <95861205+Otuslettia@users.noreply.github.com>
2023-03-14 07:20:30 +0800

    Make the ML player also consider the most possible hit bricks
    also add some conversation record with ChatGPT, I'm trying to finish this work with it.

--- Arkanoid/ml_play.py
+++ Arkanoid/ml_play.py
@@ -12,9 +12,35 @@ class MLPlay:
         self.previous_ball = (0, 0)
        
 
-        with open(os.path.join(os.path.dirname(__file__),'save','model.pickle'),'rb') as f:
+        with open(os.path.join(os.path.dirname(__file__),'model.pickle'),'rb') as f:
             self.model = pickle.load(f)
     
+    def find_collided_brick(self, x, y, s_x, s_y, bricks):
+        if bricks == None:
+            return None
+        min_distance = float('inf')
+        collided_brick = None
+        r = 2.5
+        for brick in bricks:
+            b_x, b_y = brick
+            brick_width, brick_height = 25, 10
+            distance = ((x-b_x)**2 + (y-b_y)**2)**0.5
+            if distance <= r + brick_width/2:
+                return brick
+            if (x-b_x)*s_x <= 0 or (y-b_y)*s_y <= 0:
+                continue
+            time = min(abs((x-b_x)/s_x), abs((y-b_y)/s_y))
+            collision_x = x + s_x * time
+            collision_y = y + s_y * time
+            dx = abs(collision_x - b_x) - brick_width/2
+            dy = abs(collision_y - b_y) - brick_height/2
+            if dx > r or dy > r:
+                continue
+            if time < min_distance:
+                min_distance = time
+                collided_brick = brick
+        return collided_brick
+
     def update(self, scene_info, *args, **kwargs):
         if(scene_info["status"]== "GAME_OVER" or scene_info["status"]=="GAME_PASS"):
             return "RESET"
@@ -39,7 +65,16 @@ class MLPlay:
                 else:
                     Direction = 3
             
-            x=np.array([Ball_x,Ball_y,Speed_x,Speed_y,Direction]).reshape((1,-1))
+            next_brick = [0, 0]
+            next_brick = self.find_collided_brick(
+                Ball_x, Ball_y, Speed_x, Speed_y, scene_info["bricks"].extend(scene_info["hard_bricks"]))
+            if next_brick is not None:
+                Collided_brick_x = next_brick[0]
+                Collided_brick_y = next_brick[1]
+            else:
+                Collided_brick_x = Collided_brick_y = -1
+
+            x = np.column_stack((Ball_x, Ball_y, Speed_x, Speed_y, Direction, Collided_brick_x, Collided_brick_y))
             y=self.model.predict(x)
             if scene_info["platform"][0] + 20 +5 < y:
                 command = "MOVE_RIGHT"
